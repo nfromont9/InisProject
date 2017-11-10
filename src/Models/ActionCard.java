369ef707002f19751e3789cf6afb09bf5275aeaa
fabie_000 <fabien3669@gmail.com>
2017-11-10 17:31:47 +0100

    update

---
+++ src/Models/ActionCard.java
@@ -0,0 +1,10 @@
+package Models;
+
+/**
+ * Created by fabie_000 on 10/11/2017.
+ */
+public class ActionCard extends Cards {
+    public ActionCard(String name, int jouabilitySymbol, Effet effect, boolean visible, boolean fourPlayer) {
+        super(name, jouabilitySymbol, effect, visible, fourPlayer);
+    }
+}
---
+++ src/Models/EpicStoryCard.java
@@ -0,0 +1,10 @@
+package Models;
+
+/**
+ * Created by fabie_000 on 10/11/2017.
+ */
+public class EpicStoryCard extends Cards {
+    public EpicStoryCard(String name, int jouabilitySymbol, Effet effect, boolean visible, boolean fourPlayer) {
+        super(name, jouabilitySymbol, effect, visible, fourPlayer);
+    }
+}
