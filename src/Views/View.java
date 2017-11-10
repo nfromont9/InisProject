29ec1adc4b92872348fd463769fc2739bb5f8922
FROMONT Nicolas <nicolas.fromont@edu.univ-fcomte.fr>
2017-11-10 15:11:52 +0100

    Ajout des dossiers d'images et début de la classe View

--- src/Views/View.java
+++ src/Views/View.java
@@ -24,6 +24,7 @@ public class View extends JFrame {
         JPanel panMain = new JPanel();
         panMain.add(panContainer);
 
+
         this.setContentPane(panMain);
         this.setMinimumSize(null);
         this.setPreferredSize(new Dimension(450, 260));
