b87c7df0f71a8f1add01b2303cda64088c6e6ff2
FROMONT Nicolas <nicolas.fromont@edu.univ-fcomte.fr>
2017-11-24 15:24:19 +0100

    Lobby

--- src/Models/ActionCardName.java
+++ src/Models/ActionCardName.java
@@ -1,8 +1,5 @@
 package Models;
 
-/**
- * Created by fabie_000 on 10/11/2017.
- */
 public enum ActionCardName {
     ArtisansEtPaysans("Artisans et paysans", ActionCardName.SEASON),
     Barde("Barde", ActionCardName.DOUBLE),
